f1863f0bac975347b4795e0b6726123a2b8c4fa7
schebiwot <schebiwot@gmail.com>
2019-12-07 17:59:48 +0300

    else and elif statement

--- lesson_5/leson_1.py
+++ lesson_5/leson_1.py
@@ -82,5 +82,29 @@ for x,y in student.items():
 
 #variable name=['item1','item2','item3']
 
+#if else
+
+marks=40
+score=56
+
+if score > marks:
+    print("you passed")
+else:
+    print("you failed")
+
+#elif
+
+marks=40
+score=40
+
+if score > marks:
+    print("you passed with score higher than 40")
+elif score==marks:
+    print("you passed with score eqaual to 40")
+else:
+    print("you failed")
+
+
+
 
 
